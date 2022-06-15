5f8229ca8d5cce1d6aa75e8568c8940b64232f7d
Gsak79 <gonchissn@gmail.com>
2022-06-14 23:40:17 -0500

    Hito 2: Agregar coordenadas en el grafo

--- interseccion.py
+++ interseccion.py
@@ -1,18 +1,30 @@
 class interseccion:
-    def __init__(self, calleId, nombre, origen, destino, distancia, velocidad, Xorigen, Yorigen, Xdestino, Ydestino):
+    def __init__(self, calleId, nombre, origen, destino, distancia, velocidad, origenX, origenY, destinoX, destinoY):
         self.calleId = calleId
         self.nombre = nombre
         self.origen = origen
         self.destino = destino
         self.distancia = distancia
         self.velocidad = velocidad
-        self.Xorigen = Xorigen
-        self.Ydestino = Ydestino
-        self.Yorigen = Yorigen
-        self.Xdestino = Xdestino
+        self.origenX = origenX
+        self.destinoX = destinoX
+        self.origenY = origenY
+        self.destinoY = destinoY
 
     def getOrigen(self):
         return self.origen
 
     def getDestino(self):
         return self.destino
+
+    def getOrigenX(self):
+        return self.origenX
+
+    def getOrigenY(self):
+        return self.origenY
+
+    def getDestinoX(self):
+        return self.destinoX
+
+    def getDestinoY(self):
+        return self.destinoY
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import networkx as nx
 import calle as c
 import interseccion as i
 
@@ -8,6 +9,7 @@ datosCalles = pd.read_csv("./data/Lima-calles.csv", sep=';', header=None)
 calles = []
 intersecciones = []
 conexiones = []
+ubicaciones = {}
 
 for linea in datosCalles.index:
     calles.append(c.calle(datosCalles[0][linea], datosCalles[1][linea], datosCalles[2][linea]))
@@ -16,8 +18,14 @@ for linea in datosIntersecciones.index:
     intersecciones.append(i.interseccion(datosIntersecciones[1][linea], datosIntersecciones[2][linea],
                                          datosIntersecciones[5][linea], datosIntersecciones[6][linea],
                                          datosIntersecciones[7][linea], datosIntersecciones[8][linea],
-                                         datosIntersecciones[11][linea], datosIntersecciones[13][linea],
-                                         datosIntersecciones[12][linea], datosIntersecciones[14][linea]))
+                                         datosIntersecciones[11][linea], datosIntersecciones[12][linea],
+                                         datosIntersecciones[13][linea], datosIntersecciones[14][linea]))
 
 for i in range(len(intersecciones)):
     conexiones.append((intersecciones[i].getOrigen(), intersecciones[i].getDestino()))
+    ubicaciones[intersecciones[i].getOrigen()] = (intersecciones[i].getOrigenX(), intersecciones[i].getOrigenY())
+    ubicaciones[intersecciones[i].getDestino()] = (intersecciones[i].getDestinoX(), intersecciones[i].getDestinoY())
+
+g = nx.Graph()
+g.add_nodes_from(ubicaciones.keys())
+g.add_edges_from(conexiones)
