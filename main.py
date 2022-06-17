1b667a6b72b5961819323ba6fd71110a8ec8433a
Gsak79 <gonchissn@gmail.com>
2022-06-17 10:58:56 -0500

    (fix) Hito 3: Función dijkstra añadido

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
+import hq as hq
 import pandas as pd
 import networkx as nx
 import calle as c
 import interseccion as i
 from datetime import datetime
+import heapq as hq
+from collections import defaultdict
+import math
 
 def actualizar_hora(g, intersecciones, hora):
     for i in range(len(intersecciones)):
         g[intersecciones[i].getOrigen()][intersecciones[i].getDestino()]['weight'] = intersecciones[i].getFactorTrafico(hora)
+
+def dijkstra(listaAdy, nodoInicial):
+  visitado = defaultdict(lambda: False)
+  recorrido = defaultdict(lambda: None)
+  distanciaNodoAcumulado = defaultdict(lambda: math.inf)
+  distanciaNodoAcumulado[nodoInicial] = 0
+  queue = [(0, nodoInicial)]
+  while queue:
+    pesoAcumulado, nodoActual = hq.heappop(queue)
+    if not visitado[nodoActual]:
+      visitado[nodoActual] = True
+      for nodoVecino, peso in listaAdy[nodoActual]:
+        f = pesoAcumulado + peso
+        if f < distanciaNodoAcumulado[nodoVecino]:
+          distanciaNodoAcumulado[nodoVecino] = f
+          recorrido[nodoVecino] = nodoActual
+          hq.heappush(queue, (f, nodoVecino))
+  return recorrido, distanciaNodoAcumulado
+
+def CaminoMenor(grafo, nodoInicial, nodoFinal):
+  path, distanciaNodoAcumulado = dijkstra(grafo, nodoInicial)
+  TerminoCamino = False
+  nodoAnterior = nodoFinal
+  caminoCorto = [nodoFinal]
+  while not TerminoCamino:
+    nodoAnterior = path[int(nodoAnterior)]
+    caminoCorto.insert(0, nodoAnterior)
+    if nodoAnterior == nodoInicial:
+      TerminoCamino = True
+  return caminoCorto, distanciaNodoAcumulado[nodoFinal]
 
 
 if __name__ == "__main__":
@@ -17,6 +51,7 @@ if __name__ == "__main__":
     intersecciones = []
     conexiones = []
     ubicaciones = {}
+    listaAdy = defaultdict(list)
 
     hora = int(datetime.now().strftime('%H'))
 
@@ -30,10 +65,12 @@ if __name__ == "__main__":
                                              datosIntersecciones[11][linea], datosIntersecciones[12][linea],
                                              datosIntersecciones[13][linea], datosIntersecciones[14][linea]))
 
+    '''
+    DIBUJAR EL GRAFO
     for i in range(len(intersecciones)):
         ubicaciones[intersecciones[i].getOrigen()] = (intersecciones[i].getOrigenX(), intersecciones[i].getOrigenY())
         ubicaciones[intersecciones[i].getDestino()] = (intersecciones[i].getDestinoX(), intersecciones[i].getDestinoY())
-
+    '''
     inicio = input("Ingrese el ID de la intersección de inicio: ")
     final = input("Ingrese el ID de la intersección de destino: ")
 
@@ -45,19 +82,40 @@ if __name__ == "__main__":
 
     if opcion == '1':
         for i in range(len(intersecciones)):
+            listaAdy[intersecciones[i].getOrigen()].append(
+                (intersecciones[i].getDestino(), intersecciones[i].getFactorTrafico(hora)))
+            '''
+                DIBUJAR EL GRAFO
             conexiones.append((intersecciones[i].getOrigen(), intersecciones[i].getDestino(),
                                {'weight': intersecciones[i].getFactorTrafico(hora)}))
+            '''
     elif opcion == '3':
         for i in range(len(intersecciones)):
+            listaAdy[intersecciones[i].getOrigen()].append(
+                (intersecciones[i].getDestino(), intersecciones[i].getDistancia()))
+            '''
+                DIBUJAR EL GRAFO
             conexiones.append((intersecciones[i].getOrigen(), intersecciones[i].getDestino(),
                                {'weight': intersecciones[i].getDistancia}))
+            '''
     else:
         for i in range(len(intersecciones)):
+            listaAdy[intersecciones[i].getOrigen()].append(
+                (intersecciones[i].getDestino(), intersecciones[i].getTiempo()))
+            '''
+                DIBUJAR EL GRAFO
             conexiones.append((intersecciones[i].getOrigen(), intersecciones[i].getDestino(),
                                {'weight': intersecciones[i].getTiempo()}))
+            '''
 
+    ''' 
+    DIBUJAR EL GRAFO
     g = nx.Graph()
     g.add_nodes_from(ubicaciones.keys())
     g.add_edges_from(conexiones)
+    '''
 
-    print(nx.dijkstra_path(g, source=int(inicio), target=int(final)))
+
+    path, distancia = CaminoMenor(listaAdy, int(inicio), int(final))
+
+    print(path)
